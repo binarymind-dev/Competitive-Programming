c453d14a3052c34caff82bd102882916b290b464
Snehal Thombare <snehalthombare888@gmail.com>
2026-05-24 11:18:18 +0530

    ✅ Solved: 4A Watermelon (Codeforces 800)

---
+++ Codeforces/Codeforces/800/4A_Watermelon.cpp
@@ -0,0 +1,16 @@
+// Codeforces 4A - Watermelon
+// Rating: 800
+// Language: C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int w;
+    cin >> w;
+    if(w > 2 && w % 2 == 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
